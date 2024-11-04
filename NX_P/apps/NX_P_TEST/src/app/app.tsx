d5a9890c2785f259f7724127c37a0eb35390f29a
DoltuAdrian0416 <Adria_Doltu@Trimble.com>
2024-11-04 12:44:02 +0200

    First_NX_Project

---
+++ NX_P/apps/NX_P_TEST/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="NX_P_TEST" />
+    </div>
+  );
+}
+
+export default App;
