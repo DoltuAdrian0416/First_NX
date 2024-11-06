486dfcbe0ff044a8bf01e729ae26697b3ef1040b
DoltuAdrian0416 <Adrian_Doltu@Trimble.com>
2024-11-06 13:29:08 +0200

    Finished React-Monorepo Tutorial

--- NX_P/apps/NX_P_TEST/src/app/app.tsx
+++ NX_P/apps/NX_P_TEST/src/app/app.tsx
@@ -1,10 +1,18 @@
 import NxWelcome from './nx-welcome';
+import { Route , Routes } from 'react-router-dom';
+import { Products } from '@./products';
+import { Orders } from '@./orders';
+function Home() {
+  return <h1>Home</h1>
+}
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="NX_P_TEST" />
-    </div>
+    <Routes>
+      <Route path ="/" element = {<Home />}></Route>
+      <Route path ="/products" element = {<Products />}></Route>
+      <Route path ="/Orders" element = {<Orders />}></Route>
+    </Routes>
   );
 }
 
